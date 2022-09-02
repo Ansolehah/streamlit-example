0dfefe5061b1e85a82b76bbb57cae3299a82fcbd
Ansolehah <111937024+Ansolehah@users.noreply.github.com>
2022-09-02 17:09:36 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
-
+    
 st.write("""
 # Simple Iris Flower Prediction App
 This app predicts the **Iris flower** type!
@@ -27,9 +26,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = pd.read_csv('https://raw.githubusercontent.com/feizhi98/FinalAssignment/main/IRIS.csv')
+X = iris[['sepal_length','sepal_width','petal_length','petal_width']]
+Y = iris['species']
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,11 +37,34 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+
+####dipslay in list###########
+#st.write(Y.unique().tolist())
+
+####dipslay in table###########
+st.write(Y.unique())
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+#st.write(iris.speciest_names[prediction])
+st.write(prediction)
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
+
+st.image("https://www.thespruce.com/thmb/xTuQCqR8TSPq_bb2D4yFIe54FPg=/4200x2362/smart/filters:no_upscale()/iris-flowers-plant-profile-5120188-hero-ccb44fcb101b4314b913ce5af09f9c1c.jpg")
+
+st.success("Congratulations Fei Zhi! You have completed Final Assignment (For Beginners). Let's proceed with advanced version.")
+
+def convert_df(iris):
+   return iris.to_csv().encode('utf-8')
+
+
+csv = convert_df(iris)
+
+st.download_button(
+   "Download Iris Data",
+   csv,
+   "iris.csv",
+   "text/csv",
+   key='download-csv'
+)
